9a15f26388fa426f0c2ef0c1ff1fdd7ad476c591
Kokky02 <daniel.kokay@gmail.com>
2022-07-17 19:22:16 +0200

    předělání navbaru na hlavní straně + jeho responzivita

---
+++ src/js/style.js
@@ -0,0 +1,13 @@
+const header = document.querySelector
+('.main-header');
+
+window.addEventListener('scroll', () => {
+    const scrollPos = window.scrollY;
+    if(scrollPos > 10){
+        header.classList.add('scrolled');
+    }   else {
+        header.classList.remove('scrolled');
+    }
+})
+    
+
